ad27b420d83862bcee0aa1f28e9c363103dfba9e
ConnorJAyscue <connorjayscue@gmail.com>
2024-04-01 17:42:45 -0400

    Adding basic layout of profile page and edit page

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from model.User import User
 
 app = Flask(__name__)
+
+user = User()
 
 @app.get('/')
 def index():
@@ -9,3 +12,13 @@ def index():
 @app.get('/calendar')
 def calendar():
     return render_template('calendar.html')
+
+@app.get('/profile')
+def profile():
+    full_name = user.first_name + ' ' + user.last_name
+    return render_template('profile.html', user=user, full_name=full_name)
+
+@app.get('/edit')
+def profile():
+    
+    return render_template('eidt_profile.html', user=user, full_name=full_name)
