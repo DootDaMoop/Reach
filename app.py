60ce28a3acc669c9dadd178b9ac85474262cb4cf
ConnorJAyscue <connorjayscue@gmail.com>
2024-04-03 17:19:14 -0400

    Working profile page, fake user data, basic css for profile.html

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
-from model.User import User
+from flask import request
+#from model.User import User
+class user:
+    first_name = 'Connor'
+    last_name = 'Ayscue'
+    username = 'ConnorAsq'
+    password = '123456789'
+    instagram = 'Connor_asku'
+    snapchat = ''
+    email = 'connorjayscue@gmail.com'
 
 app = Flask(__name__)
-
-user = User()
 
 @app.get('/')
 def index():
@@ -18,7 +25,17 @@ def profile():
     full_name = user.first_name + ' ' + user.last_name
     return render_template('profile.html', user=user, full_name=full_name)
 
-@app.get('/edit')
-def profile():
-    
-    return render_template('eidt_profile.html', user=user, full_name=full_name)
+#@app.get('/edit')
+#def profile():
+#    user.first_name = request.form.get('first_name')
+#    user.last_name = request.form.get('last_name')
+#    full_name = user.first_name + ' ' + user.last_name
+
+#    user.username = request.form.get('username')
+#    user.password = request.form.get('password')
+#    user.email = request.form.get('email')
+
+#    user.snapchat = request.form.get('snapchat')
+#    user.instagram = request.form.get('instagram')
+
+#    return render_template('eidt_profile.html', user=user, full_name=full_name)
